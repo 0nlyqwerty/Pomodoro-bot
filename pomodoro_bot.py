09cc3d1bbbb99e085cdb9d815d7b9df8067fd05f
beargu2 <powergaeko@gmail.com>
2020-10-20 23:16:31 +0900

    Add pomodoro command and logic

--- pomodoro_bot.py
+++ pomodoro_bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import platform
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import datetime
 
 # Get token from ./token.txt
 CURRENT_DIR_PATH = os.path.dirname(__file__)
@@ -19,12 +21,82 @@ game = discord.Game(f"Pomodoro Bot {version}")
 bot = commands.Bot(command_prefix='!', status=discord.Status.online,
                    activity=game, help_command=None)
 
+sched = AsyncIOScheduler()
+
 
 @bot.event
 async def on_ready():
     print(" ---------------------")
     print(" POMODORO BOT on_ready")
     print(" ---------------------")
+    sched.start()
+
+
+@bot.command()
+async def pmdr_help(ctx):
+    """ Pomodoro bot help command
+    Action:
+        Send Pomodoro command help message
+    """
+
+    await ctx.channel.send(
+        f"```css\n[Pomodoro Timer]\n - Start command : !pmdr_start [work_min] [break_min]\n     ex) !pmdr_start 25 "
+        f"5\n -  Stop command : !pmdr_stop```")
+
+
+@bot.command()
+async def pmdr_start(ctx, work_time: int, break_time: int):
+    """ Start pomodoro timer
+    Action:
+        Start break_time timer after work_time timer
+    Args:
+        work_time : work timer (minute)
+        break_time : break timer after work_time (minute)
+    """
+
+    if len(sched.get_jobs()) > 0:
+        await ctx.channel.send(
+            f"```css\n[⚠️Pomodoro timer already working!]\n - stop command : !pmdr_stop```")
+        return
+
+    async def break_schedule(work_time, break_time):
+        print('Enter break schedule')
+        await ctx.channel.send(
+            f"{ctx.author.mention}```css\n[🔥Break time end!] Let's work :)```")
+        work_expire_time = get_expire_time(work_time)
+        sched.add_job(work_schedule, 'date', run_date=work_expire_time, args=[work_time, break_time])
+        pass
+
+    async def work_schedule(work_time, break_time):
+        print('Enter work schedule')
+        await ctx.channel.send(
+            f"{ctx.author.mention}```css\n[🏁Work time end!] Let's break :)```")
+        break_expire_time = get_expire_time(break_time)
+        sched.add_job(break_schedule, 'date', run_date=break_expire_time, args=[work_time, break_time])
+        pass
+
+    work_expire_time = get_expire_time(work_time)
+    sched.add_job(work_schedule, 'date', run_date=work_expire_time, args=[work_time, break_time])
+
+    await ctx.channel.send(
+        f"```css\n[Work {work_time}min, Break {break_time}min] Pomodoro Timer START.\n - stop command : !pmdr_stop```")
+
+
+@bot.command()
+async def pmdr_stop(ctx):
+    """ Stop pomodoro timer
+    Action:
+        Stop pomodoro timer
+    """
+    sched.remove_all_jobs()
+    await ctx.channel.send(
+        f"```css\nPomodoro Timer START.\n - start command : !pmdr_start [work_min] [break_min]```")
+
+
+def get_expire_time(minutes: int):
+    now = datetime.datetime.now()
+    expire_time = now + datetime.timedelta(minutes=minutes)
+    return expire_time
 
 
 bot.run(TOKEN)
